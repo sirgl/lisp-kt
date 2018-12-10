18ba56d8dfae0f9f413fa4822eac97f311d42a32
a.dymova <adImova05@gmail.com>
2018-12-10 22:18:23 +0700

    add simple integration test

--- src/test/kotlin/DriverTest.kt
+++ src/test/kotlin/DriverTest.kt
@@ -1,15 +1,28 @@
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import shell.ShellCommandExecutor
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
 import java.util.stream.Stream
 
 
 class DriverTest {
+    val uut: Driver = Driver()
+    private val libraryPath = "src/main/resources/stdlib.lisp"
+    private val shellCommandExecutor: ShellCommandExecutor = ShellCommandExecutor()
 
     @ParameterizedTest
-    fun run() {
-
+    @MethodSource("testPathProvider")
+    fun run(path: Path) {
+        val mainFilePath = path.resolve("Main.lisp").toString()
+        val outputFileName = path.last().toString()
+        val workingDirectory = path.toString()
+        uut.run(Args(mainFilePath, workingDirectory, libraryPath, "src/test/resources/runtime.o",
+                workingDirectory, outputFileName))
+        val result = shellCommandExecutor.runCommand(path.toFile(), "./$outputFileName")
+//        Paths.get()
     }
 
     companion object {
